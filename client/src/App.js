2720b1abeb0776844acfada180b68199676c4597
Eli Gukovsky <gukovskye@gmail.com>
2019-11-14 13:59:50 -0500

    Add bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,51 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import 'bootstrap/dist/css/bootstrap.css';
 import logo from './logo.svg';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Here's slippi stuff
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [show, setShow] = React.useState(false);
+	const handleClose = () => setShow(false);
+	const handleOpen = () => setShow(true);
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+				Here's slippi stuff
+				</p>
+				<Button variant="primary" onClick={handleOpen}>
+				Launch demo modal
+				</Button>
+
+				<Modal show={show} onHide={handleClose}>
+					<Modal.Header closeButton>
+						<Modal.Title>Modal heading</Modal.Title>
+					</Modal.Header>
+					<Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+					<Modal.Footer>
+						<Button variant="secondary" onClick={handleClose}>
+							Close
+						</Button>
+						<Button variant="primary" onClick={handleClose}>
+							Save Changes
+						</Button>
+					</Modal.Footer>
+				</Modal>
+				<a
+				className="App-link"
+				href="https://reactjs.org"
+				target="_blank"
+				rel="noopener noreferrer"
+				>
+				Learn React
+				</a>
+			</header>
+		</div>
+		);
 }
 
 export default App;
