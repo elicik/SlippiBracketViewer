414b7d336e74ac56922e62ddde362805ff5b0796
Eli Gukovsky <gukovskye@gmail.com>
2019-11-21 16:31:04 -0500

    Server finally serves static page

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
+const path = require('path');
 const express = require('express');
 const { default: SlippiGame } = require('slp-parser-js');
 const app = express();
 
-app.get('/', function(req, res) {
-	res.send("Hello world!");
-});
+// app.get('/', function(req, res) {
+// 	res.send("Hello world!");
+// });
 
-// app.use(express.static(__dirname + '/client'));
+app.use(express.static(path.join(__dirname, '../client')));
 
 module.exports = app;
