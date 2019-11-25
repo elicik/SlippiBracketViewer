3ad5f28e39f686cc86a0ad6228d6c1d478c36169
Eli Gukovsky <gukovskye@gmail.com>
2019-11-24 23:06:04 -0500

    Begin adding smash.gg integration

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
-const path = require('path');
-const express = require('express');
-const { default: SlippiGame } = require('slp-parser-js');
+require("dotenv").config();
+const path = require("path");
+const express = require("express");
+const fetch = require("node-fetch");
+const { default: SlippiGame } = require("slp-parser-js");
+
 const app = express();
 
-app.get('/replay', function(req, res) {
+
+app.get("/replay", function(req, res) {
 	// TODO: connect with database
 	let filename = req.query.filename;
 	console.log(`Requested file: ${filename}`);
@@ -19,6 +23,67 @@ app.get('/replay', function(req, res) {
 	console.log("Game data sent to client");
 });
 
-app.use(express.static(path.join(__dirname, '../client')));
+app.get("/tournament/:id", function(req, res) {
+	let query1 = `query TournamentQuery($slug: String) {
+		tournament (slug: $slug) {
+			events {
+				id
+				name
+			}
+		}
+	}`;
+	let query2 = `query TournamentQuery($eventid: ID) {
+		event(id: $eventid) {
+			sets (page: 1, perPage: 500) {
+				nodes {
+					id
+					fullRoundText
+					displayScore
+					round
+					slots {
+						id
+						entrant {
+							name
+						}
+						prereqId
+					}
+				}
+			}
+		}
+	}`;
+	fetch("https://api.smash.gg/gql/alpha", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			"Accept": "application/json",
+			"Authorization": `Bearer ${process.env.SMASHGG_KEY}`
+		},
+		body: JSON.stringify({
+			query: query1,
+			variables: { "slug": req.params.id },
+		})
+	})
+	.then(data => data.json())
+	.then(data => data["data"]["tournament"]["events"].find(event => event["name"] === "Melee Singles")["id"])
+	.then((data) => fetch("https://api.smash.gg/gql/alpha", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			"Accept": "application/json",
+			"Authorization": `Bearer ${process.env.SMASHGG_KEY}`
+		},
+		body: JSON.stringify({
+			query: query2,
+			variables: { "eventid": data},
+		})
+	}))
+	.then(data => data.json())
+	.then(data => res.send(data));
+	// .then(data => {
+
+	// })
+});
+
+app.use(express.static(path.join(__dirname, "../client")));
 
 module.exports = app;
