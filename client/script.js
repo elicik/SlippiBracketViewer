35763299c184c8b8c4410741e37bce874cf5f5a1
Eli Gukovsky <gukovskye@gmail.com>
2019-11-25 14:31:41 -0500

    Begin working on bracket view

--- client/script.js
+++ client/script.js
@@ -1,6 +1,6 @@
-$("#submit").click(function (event) {
-	let data = $("#form").serialize();
-	$.get("/replay", data, function(resp) {
-		$("#visualizer").get(0).contentWindow.start(resp);
-	});
-});
+// $("#submit").click(function (event) {
+// 	let data = $("#form").serialize();
+// 	$.get("/replay", data, function(resp) {
+// 		$("#visualizer").get(0).contentWindow.start(resp);
+// 	});
+// });
