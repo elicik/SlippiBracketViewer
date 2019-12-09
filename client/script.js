9a1abad33dd5a03896b7d267c4e3d8204fba8b45
Eli Gukovsky <gukovskye@gmail.com>
2019-12-08 22:40:21 -0500

    Allow game switching

--- client/script.js
+++ client/script.js
@@ -1,12 +1,14 @@
 let tournament = "the-zoo-the-bear-exhibit";
-let gameNum = 1;
+let setid;
+let gamenum;
+
 
 let transform = function(set) {
 	let child0 = set.previous0 === null ? "" : `<div class="item-child">${transform(set.previous0)}</div>`;
 	let child1 = set.previous1 === null ? "" : `<div class="item-child">${transform(set.previous1)}</div>`;
 	return `<div class="item">
 		<div class="item-parent">
-			<div class="set" data-id="${set.id}" data-title="${set.fullRoundText + ": " + set.tag0 + " vs " + set.tag1}">
+			<div class="set" data-id="${set.id}" data-totalgames="${set.score0 + set.score1}" data-title="${set.fullRoundText + ": " + set.tag0 + " vs " + set.tag1}">
 				<div>${set.tag0}</div>
 				<div>${set.score0}</div>
 				<div>${set.tag1}</div>
@@ -20,9 +22,10 @@ let transform = function(set) {
 	</div>`;
 }
 
-function loadGame(id) {
+function loadGame() {
+	$("#gamenum").text(gamenum);
 	$("#visualizer").get(0).contentWindow.location.reload(true);
-	$.get("/replay", `filename=${tournament}/${id}_Game${gameNum}.slp`, function(resp) {
+	$.get("/replay", `filename=${tournament}/${setid}_Game${gamenum}.slp`, function(resp) {
 		$("#visualizer").get(0).contentWindow.start(resp);
 	});
 }
@@ -31,14 +34,24 @@ $.get("/tournament/" + tournament, function(data) {
 	$("#wrapper").html(transform(data));
 	$(".set").click(function(event) {
 		let target = $(event.currentTarget);
-		let id = target.data("id");
 		let title = target.data("title");
-		console.log(title);
-		gameNum = 1;
-		loadGame(id);
+		let totalgames = target.data("totalgames");
+		setid = target.data("id");
+		gamenum = 1;
+		$("#title").text(title);
+		let options = "";
+		for (let i = 1; i <= totalgames; i++) {
+			options += `<option value="${i}">Game ${i}</option>`
+		}
+		$("#select").html(options);
+		loadGame();
 		$("#modal").show();
 	});
 });
 $("#close").click(function(event) {
 	$("#modal").hide();
+});
+$("#select").change(function(event) {
+	gamenum = $("#select").val();
+	loadGame();
 });
