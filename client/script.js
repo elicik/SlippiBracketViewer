9ab375435adbb0428fccd210040fc8d162a6df03
Eli Gukovsky <gukovskye@gmail.com>
2019-12-09 18:30:27 -0500

    First set has no line

--- client/script.js
+++ client/script.js
@@ -5,15 +5,17 @@ let gamenum;
 let transform = function(set) {
 	let child0 = set.previous0 === null ? "" : `<div class="item-child">${transform(set.previous0)}</div>`;
 	let child1 = set.previous1 === null ? "" : `<div class="item-child">${transform(set.previous1)}</div>`;
+	let sethtml = `<div class="set" data-id="${set.id}" data-totalgames="${set.score0 + set.score1}" data-title="${set.fullRoundText + ": " + set.tag0 + " vs " + set.tag1}">
+		<div>${set.tag0}</div>
+		<div>${set.score0}</div>
+		<div>${set.tag1}</div>
+		<div>${set.score1}</div>
+	</div>`;
+	if (!(set.previous0 === null && set.previous1 === null)) {
+		sethtml = `<div class="item-parent">${sethtml}</div>`;
+	}
 	return `<div class="item">
-		<div class="item-parent">
-			<div class="set" data-id="${set.id}" data-totalgames="${set.score0 + set.score1}" data-title="${set.fullRoundText + ": " + set.tag0 + " vs " + set.tag1}">
-				<div>${set.tag0}</div>
-				<div>${set.score0}</div>
-				<div>${set.tag1}</div>
-				<div>${set.score1}</div>
-			</div>
-		</div>
+		${sethtml}
 		<div class="item-childrens">
 			${child0}
 			${child1}
