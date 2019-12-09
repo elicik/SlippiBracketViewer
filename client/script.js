e0b0c9fe4c26c6201852cd45564df19ef25187ea
Luke Gamelin <pfnpfnpfn@gmail.com>
2019-12-09 18:50:14 -0500

    Winning tags and scores are now bold

--- client/script.js
+++ client/script.js
@@ -5,11 +5,25 @@ let gamenum;
 let transform = function(set) {
 	let child0 = set.previous0 === null ? "" : `<div class="item-child">${transform(set.previous0)}</div>`;
 	let child1 = set.previous1 === null ? "" : `<div class="item-child">${transform(set.previous1)}</div>`;
+	let tag0 = `${set.tag0}`;
+	let score0 = `${set.score0}`;
+	let tag1 = `${set.tag1}`;
+	let score1 = `${set.score1}`;
+	if (set.score0 > set.score1)
+	{
+		score0 = `<b>${set.score0}</b>`;
+		tag0 = `<b>${set.tag0}</b>`;
+	}
+	else if (set.score1 > set.score0)
+	{
+		score1 = `<b>${set.score1}</b>`;
+		tag1 = `<b>${set.tag1}</b>`;
+	}
 	let sethtml = `<div class="set" data-id="${set.id}" data-totalgames="${set.score0 + set.score1}" data-title="${set.fullRoundText + ": " + set.tag0 + " vs " + set.tag1}">
-		<div>${set.tag0}</div>
-		<div>${set.score0}</div>
-		<div>${set.tag1}</div>
-		<div>${set.score1}</div>
+		<div>${tag0}</div>
+		<div>${score0}</div>
+		<div>${tag1}</div>
+		<div>${score1}</div>
 	</div>`;
 	if (!(set.previous0 === null && set.previous1 === null)) {
 		sethtml = `<div class="item-parent">${sethtml}</div>`;
